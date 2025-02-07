3301db9773e4cbb6b370bcb69fde0f039bf5bf2a
Dobroslaw Kijowski <dobo90@gmail.com>
2025-02-07 19:56:10 +0100

    Add support for PSSH box version 1
    * According to https://docs.axinom.com/services/drm/technical-articles/pssh/#versions-of-the-pssh-box
      version 1 of PSSH box contains two additional fields kid_count and kids.

--- crates/playready/src/binary_format/pssh.rs
+++ crates/playready/src/binary_format/pssh.rs
@@ -1,14 +1,24 @@
 #![allow(dead_code)]
 
-use binrw::BinRead;
+use binrw::{helpers::read_u24, BinRead};
 
 #[derive(BinRead, Debug, Clone)]
-#[br(big, assert(&pssh == b"pssh" && system_id == [0x9a, 0x04, 0xf0, 0x79, 0x98, 0x40, 0x42, 0x86, 0xab, 0x92, 0xe6, 0x5b, 0xe0, 0x88, 0x5f, 0x95]))]
+#[br(big,
+    assert(&pssh == b"pssh" &&
+        system_id == [0x9a, 0x04, 0xf0, 0x79, 0x98, 0x40, 0x42, 0x86, 0xab, 0x92, 0xe6, 0x5b, 0xe0, 0x88, 0x5f, 0x95] &&
+        [0, 1].contains(&version))
+)]
 pub struct PSSHBox {
     pub length: u32,
     pub pssh: [u8; 4],
-    pub fullbox: u32,
+    pub version: u8,
+    #[br(parse_with = read_u24)]
+    pub flags: u32,
     pub system_id: [u8; 16],
+    #[br(if(version == 1))]
+    pub kid_count: u32,
+    #[br(if(version == 1), count = kid_count)]
+    pub kids: Vec<[u8; 16]>,
     pub data_length: u32,
     pub data: PlayreadyHeader,
 }
